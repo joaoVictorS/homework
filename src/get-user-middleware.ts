a10c552e81c2341f133164f86523793ca67ce890
joao barros <jonnymcz2@gmail.com>
2024-10-19 09:08:06 -0300

    Fix=> Criação defiltro global de exceptions.

--- src/get-user-middleware.ts
+++ src/get-user-middleware.ts
@@ -1,4 +1,9 @@
-import { Injectable, NestMiddleware, Inject } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  Inject,
+  BadRequestException,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { IUserRepository } from './core/domain/repositories/user.repository';
 
@@ -9,13 +14,27 @@ export class UserMiddleware implements NestMiddleware {
   ) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const userId = req.headers['user-id'];
-    if (userId) {
-      const user = await this.userRepository.findById(Number(userId));
-      if (user) {
-        req['user'] = user;
-      }
+    console.log('UserMiddleware: Executando middleware...');
+
+    const userId = req.headers['user_id']; // Certifique-se de capturar o cabeçalho correto
+
+    // Lança uma exceção se o cabeçalho `user_id` não estiver presente
+    if (!userId) {
+      throw new BadRequestException('O cabeçalho user_id é obrigatório.');
     }
+
+    const user = await this.userRepository.findById(Number(userId));
+
+    // Lança uma exceção se o usuário não for encontrado
+    if (!user) {
+      throw new BadRequestException(
+        `Usuário com o ID ${userId} não foi encontrado.`,
+      );
+    }
+
+    // Atribui o usuário à requisição se encontrado
+    req['user'] = user;
+    console.log('UserMiddleware: Usuário encontrado e atribuído à requisição');
     next();
   }
 }
